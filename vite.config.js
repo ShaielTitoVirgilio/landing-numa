e3ceeb6b0f7f8d619aef11c04b806e79b718261e
Shaiel <st305773@fi365.ort.edu.uy>
2026-04-16 00:16:10 -0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 3000,
-    allowedHosts: 'all', // 👈 agregado
+    host: true
   },
   preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 3000,
-    allowedHosts: 'all', // 👈 clave
-  },
+    host: true,
+    allowedHosts: 'all'
+  }
 })
